5f506425c10231082e8cb2b38600ef833bab6c7f
Kirby <98219925+tekoWeMa@users.noreply.github.com>
2025-06-27 17:10:21 +0200

    Restructure JSON List

--- src/main/java/ch/kirby/Mine.java
+++ src/main/java/ch/kirby/Mine.java
@@ -21,7 +21,11 @@ public class Mine {
         final var token = System.getenv("DISCORD_CLIENT_TOKEN_MINE");
         final GatewayDiscordClient client = DiscordClientBuilder.create(token).build().login().block();
 
-        List<String> commands = List.of("ping.json", "stats.json", "leaderboard.json", "spotify.json");  //TODO Use Folder
+        List<String> commands = List.of(
+                "ping.json",
+                "stats.json",
+                "leaderboard.json",
+                "spotify.json");
         try {
             new GlobalCommandRegistrar(client.getRestClient()).registerCommands(commands);
         }
